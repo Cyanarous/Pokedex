ee0e9dbd86abbf002f4086ce39f62e17f102b8b8
Cyanarous <93853570+Cyanarous@users.noreply.github.com>
2025-07-12 15:44:06 +0800

    added pokemon typing, flavor text, weight and height. also fixed some layout issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,46 @@ import { useState } from 'react'
 
 function App() {
   const [pokemonName, setPokemonName] = useState('');
+  const [pokemonID, setPokemonID] = useState('');
+  const [pokemonSpecies, setPokemonSpecies] = useState('');
   const [pokemonSprite, setPokemonSprite] = useState('');
+  const [pokemonFlavorText, setPokemonFlavorText] = useState('');
   const [pokemonType, setPokemonType] = useState([]);
+  const [pokemonWeight, setPokemonWeight] = useState('');
+  const [pokemonHeight, setPokemonHeight] = useState('');
 
   async function fetchPokemon() {
     try{
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-
-      if(!response.ok){
+      const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`);
+      const flavorTextResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`);
+      if(!pokemonResponse.ok && !speciesResponse.ok && !flavorTextResponse.ok){
         throw new Error('Could not fetch data');
       }
-      const data = await response.json();
-      const pokemonSprite = data.sprites.front_default;
+      const pokemonData = await pokemonResponse.json();
+      const speciesData = await speciesResponse.json();
+      const flavorTextData = await flavorTextResponse.json();
+      
+      const pokemonSprite = pokemonData.sprites.front_default;
       setPokemonSprite(pokemonSprite);
 
-      const typesArray = data.types.map(typeInfo => typeInfo.type.name);
+      const pokeomonFlavorText  = flavorTextData.flavor_text_entries.find(e => e.language.name === "en");
+      setPokemonFlavorText(pokeomonFlavorText ? pokeomonFlavorText.flavor_text : '');
+
+      const pokemonID = pokemonData.id;
+      setPokemonID(pokemonID);
+
+      const typesArray = pokemonData.types.map(typeInfo => typeInfo.type.name);
       setPokemonType(typesArray);
+
+      const weight = pokemonData.weight;
+      setPokemonWeight(weight); 
+      const height = pokemonData.height;
+      setPokemonHeight(height);
+
+      const speciesGenusObj = speciesData.genera.find(gen => gen.language.name === "en");
+      const speciesGenus = speciesGenusObj ? speciesGenusObj.genus : "";
+      setPokemonSpecies(speciesGenus);
     }
     catch(error){
       console.error(error);
@@ -25,8 +49,8 @@ function App() {
   }
 
   return(
-    <div className="max-h-screen bg-gray-100">
-      <header className="bg-blue-600 w-full py-4 flex items-center justify-center md:justify-start shadow">
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-red-500 w-full flex items-center justify-center md:justify-start shadow">
         <div className="flex items-center space-x-4 md:ml-8">
           <h1 className="text-xl md:text-6xl font-bold text-white">Pokédex</h1>
           <input
@@ -34,30 +58,48 @@ function App() {
             value={pokemonName}
             onChange={e => setPokemonName(e.target.value)}
             placeholder="Search for a Pokémon"
-            className="border p-2 rounded w-48 md:w-4xl"
+            className="border p-2 rounded mr-0 w-40 md:w-2xl flex-"
           />
-          <button onClick={fetchPokemon} className="bg-green-200 text-blue-600 p-2 rounded font-bold">🔍</button>
+          <button onClick={fetchPokemon} className="bg-white-200 text-blue-600 p-2 rounded text-3xl">🔍</button>
         </div>
       </header>
-      <div className="flex justify-center">
-        <div className="bg-green-400 rounded shadow-md w-full max-w-xl flex flex-col items-center">
-          <img
-            src={pokemonSprite}
-            alt="Pokemon Sprite"
-            id="pokemonSprite"
-            style={{ display: pokemonSprite ? 'block' : 'none' }}
-            className="mt-4 w-xs h-xs object-contain"
-          />
-          <div className='flex space-x-2'>
-          {pokemonType.map((type, idx) => (
-            <h3
-              key={idx}
-              className="p-2 w-18 text-center rounded-md mt-2 text-lg font-semibold text-gray-800 bg-blue-200"
-            >
-              {type}
-            </h3>
-          ))}
+      <div className="flex justify-center flex-col items-center">
+        <div className="flex flex-col md:flex-row w-full max-w-6xl items-start">
+          <div className="bg-green-400 p-2 rounded shadow-md flex flex-col items-center relative md:mr-8 md:self-stretch md:w-1/2 mx-auto" style={{ minHeight: '300px' }}>
+            <img
+              src={pokemonSprite}
+              alt="Pokemon Sprite"
+              id="pokemonSprite"
+              style={{ display: pokemonSprite ? 'block' : 'none' }}
+              className="w-3xs h-3xs object-contain"
+            />
+            <h2 className="text-2xl font-bold text-gray-800 mt-4"> 
+              {pokemonName.toUpperCase() + ` #${pokemonID}`}
+            </h2>
+            <h3 className="text-xs font-bold text-gray-800 mt-2">{pokemonSpecies}</h3>
+            <div className='flex space-x-2'>
+              {pokemonType.map((type, idx) => (
+                <h3
+                  key={idx}
+                  className="p-2 w-28 text-center rounded-md mt-2 text-xs font-semibold text-gray-800 bg-blue-200"
+                >
+                  {type}
+                </h3>
+              ))}
+            </div>
           </div>
+          <div className="flex flex-col items-start pl-2 justify-start mt-8 md:mt-8 md:w-1/2 space-y-2 text-left">
+            <h3 className="text-lg font-semibold text-gray-800 mb-4 md:mb-8">
+              {pokemonFlavorText}
+            </h3>
+            <h3 className="text-md font-semibold text-xs text-gray-700">
+              Weight: {pokemonWeight ? `${pokemonWeight / 10}kg` : 'N/A'}
+            </h3>
+            <h3 className="text-md font-semibold text-xs text-gray-700">
+              Height: {pokemonHeight ? `${pokemonHeight / 10}m` : 'N/A'}
+            </h3>
+          </div>
+
         </div>
       </div>
     </div>
